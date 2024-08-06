96c56a4de4493886b2a7ce42fd07439570bfb23f
Leo Howell <hongwang2193@gmail.com>
2024-08-06 15:40:24 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 const LIBINJECTION_URL: &str = "https://github.com/libinjection/libinjection";
-const LIBINJECTION_COMMIT: &str = "b9fcaaf9e50e9492807b23ffcc6af46ee1f203b9";
+const LIBINJECTION_COMMIT: &str = "73268cfd85f9ee625e1d73ec2b37672bb2fd83f6";
 const BUILD_DIR_NAME: &str = "libinjection";
 
 fn clone_libinjection(build_dir: &Path, version: &str) -> Option<()> {
